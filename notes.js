36d01de5d79642c9bd7f4e7c7535e19af2a94be2
Akash Kumar Seth <kumarakashseth007@gmail.com>
2018-09-09 23:37:39 +0530

    Reading command from command line using yargs package.

--- notes.js
+++ notes.js
@@ -1,8 +1,25 @@
 console.log('Starting notes.js');
 
+let addNote = (title, body) => {
+  console.log('Adding note' , title, body);
+  return 'new note ';
+};
+
+let getAll = () => {
+  console.log("Getting All notes");
+};
+
+let getNote = (title) => {
+  console.log('Getting note', title);
+};
+
+let removeNote = (title) => {
+  console.log('Removing note', title);
+};
+
 module.exports = {
-  addNotes : () => {
-    console.log('addNote');
-    return 'new note ';
-  }
+  addNote,
+  getAll,
+  getNote,
+  removeNote
 };
